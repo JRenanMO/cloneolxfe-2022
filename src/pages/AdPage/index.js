6d4f589979e195338e7dbcf93893ec362c9ed2d8
JRenanMO <j.renan007@hotmail.com>
2023-02-18 13:57:27 -0300

    Pagina Anuncio terminada

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -3,12 +3,21 @@ React,
 { useState, 
   useEffect 
 } from 'react';
-import { PageArea, Fake } from './styled'
+import { 
+    PageArea, 
+    Fake,
+    OthersArea,
+    BraedChumb
+} from './styled'
 import { PageContainer } from '../../components/MainComponents';
-import { useParams } from 'react-router-dom';
+import { 
+    useParams,
+    Link 
+} from 'react-router-dom';
 import { Slide } from 'react-slideshow-image';
 import "react-slideshow-image/dist/styles.css";
 import useAPI from '../../helpers/OlxAPI';
+import AdItem from '../../components/partials/AdItem';
 
 const Page = () => {
     const api = useAPI();
@@ -53,6 +62,23 @@ const Page = () => {
 
     return (
         <PageContainer>
+            {adInfo.category &&
+            <BreadChumb>
+            Você está aqui:
+            <Link to="/">
+                Home
+                </Link>
+            /
+            <Link to={`/ads?state=${adInfo.stateName}`}>
+                {adInfo.stateName}
+            </Link>
+            /
+            <Link to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}>
+                {adInfo.category.name}
+            </Link>
+            / {adInfo.title}
+            </BreadChumb>
+            }
         <PageArea>
            <div className="leftSide">
             <div className="box">
@@ -91,13 +117,61 @@ const Page = () => {
            </div>
            <div className="rightSide">
             <div className="box box--padding">
-            {loading && <Fake height={20} />}  
+            {loading && <Fake height={20} />}
+            {adInfo.priceNegotiable && 
+                "Preço Negociável"
+            }
+            {!adInfo.priceNegotiable && adInfo.price &&
+                <div className="price">
+                    Preço:
+                    <span>
+                        R$ {adInfo.price}
+                    </span>
+                    </div>
+            }  
             </div>
-            <div className="box box--padding">
+
             {loading && <Fake height={50} />}
-            </div>
+            {adInfo.userInfo &&
+            <>
+              <a
+              href={`mailto:${adInfo.userInfo.email}`}
+              target="_blank"
+              rel="noreferrer"
+              className="contactSellerLink"
+            >
+                Fale com vendedor
+            </a>
+            <div className="createBy box box--padding">
+                <strong>
+                    {adInfo.user.name}
+                </strong>
+                <small>
+                    E-mail: {adInfo.user.email}
+                </small>
+                <small>
+                    Estado: {adInfo.userInfo.stateName}
+                </small>
+                </div>
+            </>
+            }
+            
            </div>
         </PageArea>
+        <OthersArea>
+            {adInfo.other &&
+            <>
+              <h2>
+                Outras ofertas do vendendor
+              </h2>
+              <div className="list">
+                {adInfo.other.map((i, k) =>
+                <AdItem key={k} data={i} />
+                )}
+              </div>
+            </>
+            }
+        </OthersArea>
         </PageContainer>
     )
 }
--- src/pages/AdPage/styled.js
+++ src/pages/AdPage/styled.js
@@ -74,6 +74,61 @@ export const PageArea = styled.div`
 
     .rightSide {
         width: 250px;
+     
+        .price span {
+            color: #0000ff;
+            display: block;
+            font-size: 27px;
+            font-weight: bold;
+        }
 
+        .contactSellerLink {
+            background-color: #0000ff;
+            color: #fff;
+            height: 30px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 4px #999;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-decoration: none;
+            margin-bottom: 20px;
+        }
+
+        .createBy strong {
+            display: block;
+        }
+
+        .createBy small {
+            display: block;
+            color: #999;
+            margin-top: 10px;
+        }
     }
-` 
+`;
+
+export const OthersArea = styled.div`
+    h2 {
+        font-size: 20px;
+    }
+
+    .list {
+        display: flex;
+
+        .adItem {
+            width: 25%;
+        }
+    }
+`;
+
+export const BreadChumb = styled.div`
+    font-size: 13px;
+    margin-top: 20px;
+
+    a {
+        display: inline-block;
+        margin: 0 5px;
+        text-decoration: underline;
+        color: #000;
+    }
+`;
